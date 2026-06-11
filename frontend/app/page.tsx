83566e0c5c6ec21cd5e364c004b6faab1d55c031
julie Rajaratnam <julierajaratnam@macbook-pro-de-julie.home>
2026-06-11 21:19:20 +0200

    feat: improve home page navigation

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,5 @@
-
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div >
-      <main className="min-h-screen bg-[#070B14] text-white flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        SpendSense 🚀
-      </h1>
-    </main>
-    </div>
-  );
+  redirect("/dashboard");
 }
